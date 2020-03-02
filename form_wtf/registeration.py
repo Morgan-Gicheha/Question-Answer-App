8121cc4f19fa83e72dd6caeec2c082784c0c2608
codeGiche <morgangicheha4@gmail.com>
2020-03-02 20:00:00 +0300

    capturing register errors

--- form_wtf/registeration.py
+++ form_wtf/registeration.py
@@ -5,6 +5,6 @@ from wtforms.validators import DataRequired,InputRequired,Length,EqualTo
 class Register_qa(FlaskForm):
     ''' this is a flaskform for the user registration'''
     name = StringField('name', validators=[InputRequired(), Length(max=20, message="name too long"), Length(min=4, message="name is too short")])
-    email = StringField("email", validators=[InputRequired()])
+    email = StringField("email", validators=[InputRequired(),Length(min=12, message="seems too short for an email")])
     password = PasswordField("password",validators=[InputRequired(),Length(min=4, message="password too short!"),EqualTo("confirm_password", message="passwords don't match!") ])
     confirm_password = PasswordField("Retype password")
